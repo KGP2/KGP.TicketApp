fadd32254871fd4d97db1f05cf41f676f1021599
Michał Wdowski <michal.wdowski99@gmail.com>
2023-03-22 01:35:00 +0100

    Fixes

--- KGP.TicketApp.Backend/Program.cs
+++ KGP.TicketApp.Backend/Program.cs
@@ -22,11 +22,8 @@ namespace KGP.TicketApp.Backend
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
             app.UseHttpsRedirection();
 
