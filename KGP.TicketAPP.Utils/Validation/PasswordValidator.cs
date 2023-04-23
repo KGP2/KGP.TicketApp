a53e11ca3a2d34b136c5cc0cb38ed86dec61736a
Piotr Kawa <piotr0589.pk@gmail.com>
2023-04-23 17:46:59 +0200

    Zaimplementowanie metod do walidacji hasła i adresu mail

--- KGP.TicketAPP.Utils/Validation/PasswordValidator.cs
+++ KGP.TicketAPP.Utils/Validation/PasswordValidator.cs
@@ -2,15 +2,54 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace KGP.TicketAPP.Utils.Validation
 {
     public class PasswordValidator : IValidator<string>
     {
-        public bool Validate(string obj, out string error)
+        public bool Validate(string input, out string error)
         {
-            error = "password is invalid";
+            error = string.Empty;
+
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                throw new Exception("Password should not be empty");
+            }
+
+            var hasNumber = new Regex(@"[0-9]+");
+            var hasUpperChar = new Regex(@"[A-Z]+");
+            var hasMiniMaxChars = new Regex(@".{8,25}");
+            var hasLowerChar = new Regex(@"[a-z]+");
+            var hasSymbols = new Regex(@"[!@#$%^&*()_+=\[{\]};:<>|./?,-]");
+
+            if (!hasLowerChar.IsMatch(input))
+            {
+                error = "Password should contain At least one lower case letter";
+                return false;
+            }
+            else if (!hasUpperChar.IsMatch(input))
+            {
+                error = "Password should contain At least one upper case letter";
+                return false;
+            }
+            else if (!hasMiniMaxChars.IsMatch(input))
+            {
+                error = "Password should not be less than 8 or greater than 25 characters";
+                return false;
+            }
+            else if (!hasNumber.IsMatch(input))
+            {
+                error = "Password should contain At least one numeric value";
+                return false;
+            }
+
+            else if (!hasSymbols.IsMatch(input))
+            {
+                error = "Password should contain At least one special case characters";
+                return false;
+            }
 
             return true;
         }
