ad825757ea1a13c43447709f271fb1ff4af079ee
Piotr Kawa <piotr0589.pk@gmail.com>
2023-05-01 17:26:57 +0200

    Zbudowanie struktury dziedziczenia

--- KGP.TicketAPP.Utils/PdfGenerator/TicketGenerator/TicketGeneratorInitData.cs
+++ KGP.TicketAPP.Utils/PdfGenerator/TicketGenerator/TicketGeneratorInitData.cs
@@ -7,10 +7,23 @@ using System.Threading.Tasks;
 
 namespace KGP.TicketApp.Utils.PdfGenerator.TicketGenerator
 {
-    public class TicketGeneratorInitData : IPdfGeneratorInitData 
+    public class TicketGeneratorInitData : IPdfGeneratorInitData
     {
         #region Fields
         public ClientDTO client;
+        #endregion
+
+        #region Interface methods
+
+        public void Init(IPdfGenerator<IPdfGeneratorInitData> generator)
+        {
+            if (generator.GetType() != typeof(TicketGenerator))
+                throw new ArgumentException("Generator should be type of TicketGenerator.");
+
+            var ticketGenerator = generator as TicketGenerator;
+
+            ticketGenerator.Client = client;
+        }
         #endregion
     }
 }
