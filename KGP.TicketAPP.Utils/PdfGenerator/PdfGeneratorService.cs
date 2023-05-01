0e5356090ffb72bb5afdb74104d042667db3b037
Piotr Kawa <piotr0589.pk@gmail.com>
2023-05-01 19:33:08 +0200

    Skończone generowanie biletu

---
+++ KGP.TicketAPP.Utils/PdfGenerator/PdfGeneratorService.cs
@@ -0,0 +1,20 @@
+﻿using KGP.TicketApp.Utils.PdfGenerator.PdfTicketGenerator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KGP.TicketApp.Utils.PdfGenerator
+{
+    public class PdfGeneratorService
+    {
+        #region Fields
+        private TicketGenerator ticketGenerator = new TicketGenerator();
+        #endregion
+
+        #region Properties
+        public TicketGenerator TicketGenerator => ticketGenerator;
+        #endregion
+    }
+}
---
+++ KGP.TicketApp.Backend.Tests/Utils/PdfGenerator/GeneratorTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KGP.TicketApp.Backend.Tests.Utils.PdfGenerator
+{
+    public class GeneratorTests
+    {
+        public void SaveLocally(byte[] data)
+        {
+            using var stream = File.Create("generatedPdf");
+            stream.Write(data, 0, data.Length);
+        }
+    }
+}
