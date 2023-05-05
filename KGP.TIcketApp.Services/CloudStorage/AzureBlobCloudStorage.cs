d742ffa5f214507d3a7a721bc6afae102903657a
Michał Wdowski <michal.wdowski99@gmail.com>
2023-05-06 00:46:55 +0200

    WIP

---
+++ KGP.TIcketApp.Services/CloudStorage/AzureBlobCloudStorage.cs
@@ -0,0 +1,29 @@
+﻿using Azure.Storage.Blobs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KGP.TicketApp.Services.CloudStorage
+{
+    public class AzureBlobCloudStorage : ICloudStorage
+    {
+        private BlobContainerClient client;
+
+        public AzureBlobCloudStorage(string azureStorageName, string azureStorageKey)
+        {
+            client = new BlobContainerClient("", "");
+        }
+
+        public byte[] Download(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Upload(string fileName, byte[] data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
