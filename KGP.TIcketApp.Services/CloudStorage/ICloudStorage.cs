9586d82baa1446e275e19519ea5c8fbf8952f87b
Michał Wdowski <michal.wdowski99@gmail.com>
2023-05-06 00:46:55 +0200

    WIP

---
+++ KGP.TIcketApp.Services/CloudStorage/ICloudStorage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KGP.TicketApp.Services.CloudStorage
+{
+    public interface ICloudStorage
+    {
+        byte[] Download(string path);
+        string Upload(string fileName, byte[] data);
+    }
+}
---
+++ KGP.TIcketApp.Services/CloudStorage/ServiceExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Azure.Storage.Blobs.Specialized;
+
+namespace KGP.TicketApp.Services.CloudStorage
+{
+    public static class ServiceExtensions
+    {
+        public static void AddAzureBlobCloudStorage(this IServiceCollection services, string azureStorageName, string azureStorageKey)
+        {
+            services.AddScoped<ICloudStorage, AzureBlobCloudStorage>(s =>
+            {
+                return new AzureBlobCloudStorage(azureStorageName, azureStorageKey);
+            });
+        }
+    }
+}
