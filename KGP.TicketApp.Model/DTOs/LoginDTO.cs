0ad8611c77818f1e0e9795fbabdf130692d03c2a
Michał Wdowski <michal.wdowski99@gmail.com>
2023-05-20 22:20:53 +0200

    Add response object with token

---
+++ KGP.TicketApp.Model/DTOs/LoginDTO.cs
@@ -0,0 +1,11 @@
+﻿using KGP.TicketApp.Model.Database.Tables;
+
+namespace KGP.TicketApp.Model.DTOs
+{
+    public record LoginDTO<TUser> where TUser : class
+    {
+        public string Token { get; set; } = null!;
+        public DateTime? ExpiresAt { get; set; }
+        public TUser User { get; set; } = null!;
+    }
+}
--- KGP.TicketApp.Model/DTOs/TicketDTO.cs
+++ KGP.TicketApp.Model/DTOs/TicketDTO.cs
@@ -2,7 +2,6 @@
 
 namespace KGP.TicketApp.Model.DTOs
 {
-    // TODO: model from database (generated with EF)
     public record TicketDTO
     {
         public string? TicketId { get; set; }
