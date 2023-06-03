7c98eddd559673857d4fb57588b8a76174c6b705
Piotr Kawa <piotr0589.pk@gmail.com>
2023-06-03 23:20:37 +0200

    Połączenie Bloba z backenem

---
+++ KGP.TicketAPP.Utils/Services/SimpleBlobService.cs
@@ -0,0 +1,53 @@
+﻿using Azure.Storage.Blobs;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KGP.TicketApp.Utilities.Services
+{
+    public class SimpleBlobService
+    {
+        #region Fields
+
+        private BlobServiceClient blobServiceClient;
+        private BlobContainerClient ticketCointainer;
+
+        #endregion
+
+        #region Constructors
+
+        public SimpleBlobService(string connectionString, string ticketCointainerName)
+        {
+            if (connectionString.IsNullOrEmpty())
+                throw new Exception("Blob connection string is empty. Check Azure configuration.");
+            if (ticketCointainerName.IsNullOrEmpty())
+                throw new Exception("Blob cointainer name string is empty. Check Azure configuration.");
+
+            blobServiceClient = new BlobServiceClient(connectionString);
+            ticketCointainer = blobServiceClient.GetBlobContainerClient(ticketCointainerName);
+        }
+
+        #endregion
+
+        #region Public methods
+
+
+        public string SaveTicket(byte[] ticket, string ticketName)
+        {
+            BlobClient blobClient = ticketCointainer.GetBlobClient(ticketName);
+            
+
+            using (MemoryStream memoryStream = new MemoryStream(ticket))
+            {
+                blobClient.Upload(memoryStream, overwrite: true);
+            }
+
+            return blobClient.Uri.ToString();
+        }
+
+        #endregion
+    }
+}
--- KGP.TicketApp.Backend/Options/ApplicationOptions.cs
+++ KGP.TicketApp.Backend/Options/ApplicationOptions.cs
@@ -5,8 +5,10 @@ namespace KGP.TicketApp.Backend.Options
     public record ApplicationOptions
     {
         public string DatabaseConnectionString { get; set; } = null!;
+        public string BlobConnectionString { get; set; } = null!;
         public string JwtKey { get; set; } = null!;
         public string JwtIssuer { get; set; } = null!;
         public HashAlgorithmType HashAlgorithm { get; set; }
+        public string TicketsCointainerName { get; set; } = null!;
     }
 }
