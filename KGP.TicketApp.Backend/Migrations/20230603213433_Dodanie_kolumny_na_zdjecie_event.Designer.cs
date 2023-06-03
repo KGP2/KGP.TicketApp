c4b341bffc1a3d94ec2b36f3ffe875c945f44873
Piotr Kawa <piotr0589.pk@gmail.com>
2023-06-03 23:34:54 +0200

    Dodanie kolumny na zdjęcia

--- KGP.TicketApp.Backend/Migrations/20230603213433_Dodanie_kolumny_na_zdjecie_event.Designer.cs
+++ KGP.TicketApp.Backend/Migrations/20230603213433_Dodanie_kolumny_na_zdjecie_event.Designer.cs
@@ -4,6 +4,7 @@ using KGP.TicketApp.Model.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace KGP.TicketApp.Backend.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20230603213433_Dodanie_kolumny_na_zdjecie_event")]
+    partial class Dodanie_kolumny_na_zdjecie_event
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
